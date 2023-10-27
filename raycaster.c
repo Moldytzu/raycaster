67da9975ed52e5d4100d279e20e50bb2970e064d
Moldytzu <m.alex123@yahoo.com>
2023-10-27 11:22:00 +0300

    add texture mapping

--- raycaster.c
+++ raycaster.c
@@ -39,26 +39,48 @@ void playerUpdateDelta()
     // becuase playerAngle is a normalised vector, we can use playerSpeed as a scaler
 }
 
+// helpers
 #define TO_RADIANS(x) ((x) * (PI / 180.0))
+#define FRACTIONAL_OF(x) ((x)-(int)(x))
+
+// settings
 #define FOV 60.0 // in degrees
 #define RESOLUTION 10
 #define MAX_DISTANCE 10000
 #define RAY_PER_DEG (WINDOW_WIDTH / FOV * RESOLUTION)
 #define DISTANCE_COEFFICIENT (0.005 / TO_RADIANS(FOV))
-#define SHADE_COEFFICIENT 500
+#define SHADE_COEFFICIENT 400
 #define MAX_WALL_HEIGHT WINDOW_HEIGHT
 #define WALL_HEIGHT (MAX_WALL_HEIGHT / 2)
 #define MAX_FPS 1000
+
+#define TEXTURE_WIDTH 10
+#define TEXTURE_HEIGHT 10
+double wallTexture[TEXTURE_WIDTH][TEXTURE_HEIGHT] = {
+    {0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9},
+    {0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9},
+    {0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9},
+    {0,0.1,0.2,0.3,0.4,0.0,0.6,0.7,0.8,0.9},
+    {0,0.1,0.2,0.3,0.4,0.0,0.6,0.7,0.8,0.9},
+    {0,0.1,0.2,0.3,0.4,0.0,0.6,0.7,0.8,0.9},
+    {0,0.1,0.2,0.3,0.0,0.0,0.0,0.7,0.8,0.9},
+    {0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9},
+    {0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9},
+    {0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9},
+};
+
 void drawRays()
 {
-    double rayAngle, wallX, wallY, rayX, rayY, h, distance, lineHeight, lineOffset, cameraAngle, shadeDistance;
+    double rayAngle, wallX, wallY, rayX, rayY, h, distance, lineHeight, lineOffset, cameraAngle, shadeDistance, shading;
+    int wallTextureX, wallTextureY;
+    bool hitRight, hitLeft; 
     for (double ray = 0; ray < FOV * RAY_PER_DEG; ray++) // we want to raycast FOV * RAYS_PER_DEG rays
     {
         rayAngle = playerAngle + TO_RADIANS(ray / RAY_PER_DEG - FOV / 2); // set the ray angle derived from the ray index
         wallX = playerX; // set current wall coordinates to player's
         wallY = playerY; //
         rayY = sin(rayAngle) * DISTANCE_COEFFICIENT; // use vector decomposition to determine X and Y components of the ray
-        rayX = cos(rayAngle) * DISTANCE_COEFFICIENT; // 
+        rayX = cos(rayAngle) * DISTANCE_COEFFICIENT; //
 
         for(distance = 0; distance < MAX_DISTANCE; distance++) // check for ray collision
         {
@@ -69,6 +91,16 @@ void drawRays()
                 wallX >= 0 && wallY >= 0 &&
                 mapWalls[(int)wallY * mapHeight + (int)wallX]) // check for wall to be present
             {
+                // if wallX coordinate is very close to being an integer, it means it's very close to the edge and we're just casting in the side 
+                hitRight = FRACTIONAL_OF(wallX) > 0.99;
+                hitLeft = FRACTIONAL_OF(wallX) < 0.01;  
+
+                if(hitRight || hitLeft)                        // if we've hit the side
+                    wallTextureX = FRACTIONAL_OF(wallY) * 100; // we will use the Y fraction as the X index
+                else                                           // or else if we've hit the front/back
+                    wallTextureX = FRACTIONAL_OF(wallX) * 100; // we will use th X fraction as the X index
+
+                wallTextureX /= TEXTURE_WIDTH;
                 break;
             }
         }
@@ -83,16 +115,29 @@ void drawRays()
 
         // draw the ray on the map
         shadeDistance = 1 / distance;
+        shading = shadeDistance * SHADE_COEFFICIENT;
+
+        if(shading >= 1) // clamp the shade to 1
+            shading = 1;
 
         glLineWidth(1 / RESOLUTION);
         glBegin(GL_LINES);
-        glColor3f(shadeDistance * SHADE_COEFFICIENT, 0, 0); // wall
-        glVertex2f(ray / RESOLUTION, lineHeight + lineOffset);
-        glVertex2f(ray / RESOLUTION, lineOffset);
 
-        glColor3f(0, shadeDistance * SHADE_COEFFICIENT, 0); // floor
+        // wall drawing    
+        double z = lineHeight / TEXTURE_HEIGHT;
+        for(double drawY = 0; drawY < TEXTURE_HEIGHT; drawY++)
+        {
+            wallTextureY = TEXTURE_HEIGHT - 1 - (int)drawY;
+            glColor3f(wallTexture[wallTextureY][wallTextureX] * shading,0,0);
+            glVertex2f(ray / RESOLUTION, drawY * z + lineOffset);
+            glVertex2f(ray / RESOLUTION, (drawY + 1) * z + lineOffset);
+        }
+
+        // floor drawing
+        glColor3f(0, shading, 0);
         glVertex2f(ray / RESOLUTION, lineOffset);
         glVertex2f(ray / RESOLUTION, 0);
+
         glEnd();
     }
 }
