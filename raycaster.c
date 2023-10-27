8de8a25136211d65446249a1430b0005229c23a6
Moldytzu <m.alex123@yahoo.com>
2023-10-27 18:53:17 +0300

    properly clamp playerY angle

--- raycaster.c
+++ raycaster.c
@@ -15,6 +15,13 @@
         if (x > 2 * PI)        \
             x -= 2 * PI;       \
     } // wrap x degree around radians values (0<=x<=2PI)
+#define CLAMP_TO_RADIANS(x) \
+    {                       \
+        if (x < 0)          \
+            x = 0;          \
+        if (x > 2 * PI)     \
+            x = 2 * PI;     \
+    }
 
 double playerX = 1, playerY = 1, playerDx, playerDy, playerYAngle = 1.5707 /*90 deg in radians*/, playerXAngle = 0.523 /*30 deg in radians*/, playerSpeed = 1.5, playerRotationDegrees = 100;
 double deltaTime = 0;
@@ -22,14 +29,14 @@ bool keyWalkForwards = false, keyWalkBackwards = false, keyLookLeft = false, key
 
 const int mapWidth = 8, mapHeight = 8;
 int mapWalls[8][8] = {
-    {1,1,1,1,1,1,1,1},
-    {1,0,0,0,0,0,0,1},
-    {1,0,0,1,0,0,0,1},
-    {1,0,0,0,0,0,0,1},
-    {1,0,0,0,0,1,0,1},
-    {1,0,1,0,0,1,0,1},
-    {1,0,1,0,0,1,0,1},
-    {1,1,1,1,1,1,1,1},
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 1, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 1, 0, 1},
+    {1, 0, 1, 0, 0, 1, 0, 1},
+    {1, 0, 1, 0, 0, 1, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1},
 };
 
 // Update/Draw
@@ -42,11 +49,11 @@ void playerUpdateDelta()
 
 // helpers
 #define TO_RADIANS(x) ((x) * (PI / 180.0))
-#define FRACTIONAL_OF(x) ((x)-(int)(x))
+#define FRACTIONAL_OF(x) ((x) - (int)(x))
 
 // settings
 #define FOV 75.0 // in degrees
-#define RESOLUTION 1
+#define RESOLUTION 4
 #define MAX_DISTANCE 10000
 #define RAY_PER_DEG (WINDOW_WIDTH / FOV * RESOLUTION)
 #define DISTANCE_COEFFICIENT (0.005 / TO_RADIANS(FOV))
@@ -58,32 +65,32 @@ void playerUpdateDelta()
 #define TEXTURE_WIDTH 10
 #define TEXTURE_HEIGHT 10
 double wallTexture[TEXTURE_WIDTH][TEXTURE_HEIGHT] = {
-    {0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9},
-    {0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9},
-    {0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9},
-    {0,0.1,0.2,0.3,0.4,0.0,0.6,0.7,0.8,0.9},
-    {0,0.1,0.2,0.3,0.4,0.0,0.6,0.7,0.8,0.9},
-    {0,0.1,0.2,0.3,0.4,0.0,0.6,0.7,0.8,0.9},
-    {0,0.1,0.2,0.3,0.0,0.0,0.0,0.7,0.8,0.9},
-    {0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9},
-    {0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9},
-    {0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9},
+    {0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9},
+    {0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9},
+    {0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9},
+    {0, 0.1, 0.2, 0.3, 0.4, 0.0, 0.6, 0.7, 0.8, 0.9},
+    {0, 0.1, 0.2, 0.3, 0.4, 0.0, 0.6, 0.7, 0.8, 0.9},
+    {0, 0.1, 0.2, 0.3, 0.4, 0.0, 0.6, 0.7, 0.8, 0.9},
+    {0, 0.1, 0.2, 0.3, 0.0, 0.0, 0.0, 0.7, 0.8, 0.9},
+    {0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9},
+    {0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9},
+    {0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9},
 };
 
 void drawRays()
 {
     double rayAngle, wallX, wallY, rayX, rayY, h, distance, lineHeight, lineOffset, cameraAngle, shadeDistance, shading;
     int wallTextureX, wallTextureY;
-    bool hitRight, hitLeft; 
+    bool hitRight, hitLeft;
     for (double ray = 0; ray < FOV * RAY_PER_DEG; ray++) // we want to raycast FOV * RAYS_PER_DEG rays
     {
         rayAngle = playerXAngle + TO_RADIANS(ray / RAY_PER_DEG - FOV / 2); // set the ray angle derived from the ray index
-        wallX = playerX; // set current wall coordinates to player's
-        wallY = playerY; //
-        rayY = sin(rayAngle) * DISTANCE_COEFFICIENT; // use vector decomposition to determine X and Y components of the ray
-        rayX = cos(rayAngle) * DISTANCE_COEFFICIENT; //
+        wallX = playerX;                                                   // set current wall coordinates to player's
+        wallY = playerY;                                                   //
+        rayY = sin(rayAngle) * DISTANCE_COEFFICIENT;                       // use vector decomposition to determine X and Y components of the ray
+        rayX = cos(rayAngle) * DISTANCE_COEFFICIENT;                       //
 
-        for(distance = 0; distance < MAX_DISTANCE; distance++) // check for ray collision
+        for (distance = 0; distance < MAX_DISTANCE; distance++) // check for ray collision
         {
             wallX = wallX + rayX; // increase wall coordinates
             wallY = wallY + rayY;
@@ -92,11 +99,11 @@ void drawRays()
                 (int)wallX >= 0 && (int)wallY >= 0 &&
                 mapWalls[(int)wallY][(int)wallX]) // check for wall to be present
             {
-                // if wallX coordinate is very close to being an integer, it means it's very close to the edge and we're just casting in the side 
+                // if wallX coordinate is very close to being an integer, it means it's very close to the edge and we're just casting in the side
                 hitRight = FRACTIONAL_OF(wallX) > 0.99;
-                hitLeft = FRACTIONAL_OF(wallX) < 0.01;  
+                hitLeft = FRACTIONAL_OF(wallX) < 0.01;
 
-                if(hitRight || hitLeft)                        // if we've hit the side
+                if (hitRight || hitLeft)                       // if we've hit the side
                     wallTextureX = FRACTIONAL_OF(wallY) * 100; // we will use the Y fraction as the X index
                 else                                           // or else if we've hit the front/back
                     wallTextureX = FRACTIONAL_OF(wallX) * 100; // we will use th X fraction as the X index
@@ -112,24 +119,24 @@ void drawRays()
         distance = distance * cos(cameraAngle); // adjust distance by x component of camera angle
 
         lineHeight = WALL_HEIGHT * MAX_WALL_HEIGHT / distance;
-        lineOffset = WALL_HEIGHT - lineHeight/2 + WINDOW_HEIGHT * cos(playerYAngle); // move the line at middle and modify its offset based on the player vertical angle
+        lineOffset = WALL_HEIGHT - lineHeight / 2 + WINDOW_HEIGHT * cos(playerYAngle); // move the line at middle and modify its offset based on the player vertical angle
 
         // draw the ray on the map
         shadeDistance = 1 / distance;
         shading = shadeDistance * SHADE_COEFFICIENT;
 
-        if(shading >= 1) // clamp the shade to 1
+        if (shading >= 1) // clamp the shade to 1
             shading = 1;
 
         glLineWidth(1 / RESOLUTION);
         glBegin(GL_LINES);
 
-        // wall drawing    
+        // wall drawing
         double z = lineHeight / TEXTURE_HEIGHT;
-        for(double drawY = 0; drawY < TEXTURE_HEIGHT; drawY++)
+        for (double drawY = 0; drawY < TEXTURE_HEIGHT; drawY++)
         {
             wallTextureY = TEXTURE_HEIGHT - 1 - (int)drawY;
-            glColor3f(wallTexture[wallTextureY][wallTextureX] * shading,0,0);
+            glColor3f(wallTexture[wallTextureY][wallTextureX] * shading, 0, 0);
             glVertex2f(ray / RESOLUTION, drawY * z + lineOffset);
             glVertex2f(ray / RESOLUTION, (drawY + 1) * z + lineOffset);
         }
@@ -139,7 +146,7 @@ void drawRays()
         glVertex2f(ray / RESOLUTION, lineOffset);
         glVertex2f(ray / RESOLUTION, 0);
 
-        // ceiling 
+        // ceiling
         glColor3f(0, 0, shading * 0.8);
         glVertex2f(ray / RESOLUTION, WINDOW_HEIGHT);
         glVertex2f(ray / RESOLUTION, lineHeight + lineOffset);
@@ -150,51 +157,53 @@ void drawRays()
 
 void inputUpdate()
 {
-    if(keyLookRight)
+    if (keyLookRight)
     {
         playerXAngle += TO_RADIANS(playerRotationDegrees * deltaTime);
         WRAP_AROUND_RADIANS(playerXAngle);
         playerUpdateDelta();
     }
 
-    if(keyLookLeft)
+    if (keyLookLeft)
     {
         playerXAngle -= TO_RADIANS(playerRotationDegrees * deltaTime);
         WRAP_AROUND_RADIANS(playerXAngle);
         playerUpdateDelta();
     }
 
-    if(keyWalkBackwards)
+    if (keyWalkBackwards)
     {
         // do the vector addition and subtraction
         playerY -= playerDy * playerSpeed * deltaTime;
         playerX -= playerDx * playerSpeed * deltaTime;
     }
 
-    if(keyWalkForwards)
+    if (keyWalkForwards)
     {
         // do the vector addition and subtraction
         playerY += playerDy * playerSpeed * deltaTime;
         playerX += playerDx * playerSpeed * deltaTime;
     }
 
-    if(keyLookUp)
+    if (keyLookUp)
     {
         playerYAngle += TO_RADIANS(playerRotationDegrees * deltaTime);
-        WRAP_AROUND_RADIANS(playerYAngle);
+        if (playerYAngle >= PI)
+            playerYAngle = PI;
     }
 
-    if(keyLookDown)
+    if (keyLookDown)
     {
         playerYAngle -= TO_RADIANS(playerRotationDegrees * deltaTime);
-        WRAP_AROUND_RADIANS(playerYAngle);
+        if (playerYAngle <= 0)
+            playerYAngle = 0;
     }
 }
 
 // Callbacks
 void windowUpdate()
 {
-    int a =  glutGet(GLUT_ELAPSED_TIME);
+    int a = glutGet(GLUT_ELAPSED_TIME);
 
     inputUpdate();
     drawRays();        // draw rays on screen
@@ -203,10 +212,10 @@ void windowUpdate()
     do
     {
         int b = glutGet(GLUT_ELAPSED_TIME);
-        deltaTime = (b-a) * 1/1000.0; 
-    } while((1/deltaTime) > MAX_FPS && MAX_FPS);
+        deltaTime = (b - a) * 1 / 1000.0;
+    } while ((1 / deltaTime) > MAX_FPS && MAX_FPS);
 
-    printf("FPS: %f\n",1/deltaTime);
+    printf("FPS: %f\n", 1 / deltaTime);
 
     glutPostRedisplay(); // redraw window
 }
@@ -237,7 +246,7 @@ void windowKeyboardUp(char key, int x, int y)
         break;
     }
 
-    glutPostRedisplay(); // tell glut we're done 
+    glutPostRedisplay(); // tell glut we're done
 }
 
 void windowKeyboard(char key, int x, int y)
