285a5dd05b82075eb02e24deced229eb6acb00df
Moldytzu <m.alex123@yahoo.com>
2023-10-27 21:43:03 +0300

    enable all warnings

--- buildrun.sh
+++ buildrun.sh
@@ -1,6 +1,6 @@
 SRC="src/raycaster.c src/player.c src/globals.c src/window.c"
 LIBS="-lGL -lGLU -lglut -lm"
-CFLAGS="-Ofast -g -Isrc/"
+CFLAGS="-W -Wall -Wextra -Wpedantic -Ofast -g -Isrc/"
 
 set -e # break on error
 gcc $CFLAGS $SRC $LIBS -o ray
